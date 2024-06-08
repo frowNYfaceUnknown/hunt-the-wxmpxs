0634d3cc7fa1bcf7712c846d6ba8dccbe39637e5
Aarya Parekh <aarya.p2@ahduni.edu.in>
2024-06-09 01:26:00 +0530

    Simple Agent: Finished Manual Agent

--- src/agent/ManualAgent.py
+++ src/agent/ManualAgent.py
@@ -4,4 +4,19 @@ class ManualAgent(Agent):
 
     def hunt(self) -> None:
         
-        pass ## for now
+        while self.isAlive and not self.hasExited:
+            
+            ## display the grid first
+            self.grid.disp_grid()
+
+            ## perceive the environment and then display the perception
+            self.perceive()
+            self.disp_perception()
+            
+            ## ask user what to do
+            mov = input("What does the agent do next? (W | A | S | D | E | Q | H):").casefold()
+            self.decipher_input(mov)
+
+            ## display the grid one last time if the player died.
+            if not self.isAlive:
+                self.grid.disp_grid()
